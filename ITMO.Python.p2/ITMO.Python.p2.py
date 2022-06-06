46209b0b2f9d0bc1d1207d55294ee3e1cd156abb
AydarSE <Galiev_aidar@mail.ru>
2022-06-06 21:19:45 +0300

    Update

---
+++ ITMO.Python.p2/ITMO.Python.p2.py
@@ -0,0 +1,12 @@
+from turtle import title
+
+
+string1 = "This is a string."
+string2 = " This is another string."
+print("#2","\n", string1+string2)
+print(len(string1))
+print(string1.title())
+print(string2.lower())
+
+
+
